775e49093b7adf444638fe3623800b5c8a7b0fcc
Omar Villafuerte <ovillafuerte.94@gmail.com>
2023-09-15 12:36:04 -0400

    Add contact form v1

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -3,16 +3,30 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
 {
-    #[Route('/page', name: 'app_page')]
-    public function index(): Response
+    #[Route('/contact-v1', methods: ['GET', 'POST'])]
+    public function contactV1(): Response
     {
-        return $this->render('page/index.html.twig', [
-            'controller_name' => 'PageController',
+        $form = $this->createFormBuilder()
+            ->add('email', TextType::class)
+            ->add('message', TextareaType::class, [
+                'label' => 'Comment, suggestion or message'
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Sent'
+            ])
+            ->setMethod('POST')
+            ->getForm();
+
+        return $this->render('page/contact-v1.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }

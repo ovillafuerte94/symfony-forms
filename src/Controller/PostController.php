eca95f145f44ff2b34f96345c3edd486abe0f42e
Omar Villafuerte <ovillafuerte.94@gmail.com>
2023-09-15 19:09:34 -0400

    Add action for save post

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,16 +3,27 @@
 namespace App\Controller;
 
 use App\Form\PostType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PostController extends AbstractController
 {
     #[Route('/post/create', name: 'post_create')]
-    public function create(): Response
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(PostType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            $entityManager->persist($form->getData());
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Post successfully saved');
+            return $this->redirectToRoute('post_create');
+        }
 
         return $this->render('post/create.html.twig', [
             'form' => $form->createView(),

08e1cff96a18dbcdcbaea6340691becd788a4a4f
Omar Villafuerte <ovillafuerte.94@gmail.com>
2023-09-15 17:42:23 -0400

    Add enhancements to the post creation form

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -6,15 +6,26 @@ use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class PostType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('body')
-        ;
+            ->add('title', TextType::class, [
+                'label' => 'Publication title',
+                'help'  => 'Think SEO How would you search in Google?'
+            ])
+            ->add('body', TextareaType::class, [
+                'label' => 'Content',
+                'attr'  => ['rows' => 9, 'class' => 'bg-light']
+            ])
+            ->add('Send', SubmitType::class, [
+                'attr' => ['class' => 'btn-dark']
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void

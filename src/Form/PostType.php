1020f26da44e007cd0f6d70da64bb0c734df0873
Omar Villafuerte <ovillafuerte.94@gmail.com>
2023-09-15 18:16:31 -0400

    Add choice control in the posts form

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
@@ -15,6 +16,22 @@ class PostType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('category', ChoiceType::class, [
+                'choices' => [
+                    //'PHP'     => 'php',
+                    //'Laravel' => 'laravel',
+                    //'Symfony' => 'symfony'
+                    'Languages' => [
+                        'PHP' => 'php'
+                    ],
+                    'Frameworks' => [
+                        'Laravel' => 'laravel',
+                        'Symfony' => 'symfony'
+                    ]
+                ],
+                'placeholder' => 'Please select one...',
+                'label' => 'Categories'
+            ])
             ->add('title', TextType::class, [
                 'label' => 'Publication title',
                 'help'  => 'Think SEO How would you search in Google?'
